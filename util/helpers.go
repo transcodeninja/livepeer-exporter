427b6327eb43f92253cffa7de5d890098b8ccc6f
rickstaa <rick.staa@outlook.com>
2023-11-14 11:56:29 +0100

    fix: fix reward call ratio calculation
    This commit ensures the reward call ratio does not show 0% when
    something goes wrong while parsing the data returned from the
    orchestrator info API.

--- util/helpers.go
+++ util/helpers.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"math"
 	"strconv"
-
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 // BoolToFloat64 converts a bool to a float64.
@@ -18,29 +16,33 @@ func BoolToFloat64(b bool) float64 {
 	return 0.0
 }
 
+// Round rounds a float64 to a given number of decimal places.
+func Round(value float64, decimals int) float64 {
+	shift := math.Pow(10, float64(decimals))
+	return math.Round(value*shift) / shift
+}
+
 // StringToFloat64 parses a string to a float64.
 // If the string cannot be parsed, it returns an error.
 func StringToFloat64(s string) (float64, error) {
 	f, err := strconv.ParseFloat(s, 64)
 	if err != nil {
 		log.Printf("Error parsing value %v: %v", s, err)
-		return 0.0, err
 	}
-	return f, nil
+	return f, err
 }
 
-// ParseFloatAndSetGauge parses a string to a float64 and sets the value of the given gauge.
-func ParseFloatAndSetGauge(value string, gauge prometheus.Gauge) {
-	parsed, err := StringToFloat64(value)
+// SetFloatFromStr sets the value of a float64 pointer from a string.
+// If the string cannot be parsed to a float64, it logs an error and returns.
+// If significance is not -1, it rounds the float64 to the given number of decimal places.
+func SetFloatFromStr(dest *float64, source string, significance int) {
+	temp, err := StringToFloat64(source)
 	if err != nil {
-		log.Printf("Error parsing value %v: %v", value, err)
+		log.Printf("Error parsing string to float: %v", err)
 		return
 	}
-	gauge.Set(parsed)
-}
-
-// Round rounds a float64 to a given number of decimal places.
-func Round(value float64, decimals int) float64 {
-	shift := math.Pow(10, float64(decimals))
-	return math.Round(value*shift) / shift
+	if significance != -1 {
+		temp = Round(temp, significance)
+	}
+	*dest = temp
 }
